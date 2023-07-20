94d41891f6e624f587ad843a0998560a7fb46d26
Sagar-codes <paulsagar5433@gmail.com>
2023-07-20 11:21:42 +0530

    added quote details

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from yahoo_fin.stock_info import *
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def stockTracker(request):
     stockPicker = tickers_nifty50()
-    print(stockPicker)
-    return render(request, 'mainapp/home.html', {'stockPicker':stockPicker})
+    # print(stockPicker)
+    return render(request, 'mainapp/home.html', {'stockPicker': stockPicker})
+
 
 def liveStocks(request):
+    stockpicker = request.GET.getList('stockpicker')
+    print(stockpicker)
+    data = {}
+    available_stocks = tickers_nifty50()
+    for i in stockpicker:
+        details = get_quote_table(i)
+        data.update(details)
+        if i in available_stocks:
+            pass
+        else:
+            return HttpResponse("No Stocks Found")
+        print(data)
     return render(request, 'mainapp/stocks.html')
